ebfee3f60e6cf607babe20539f911608b9969008
Ronan Pickell <ronanpickell@gmail.com>
2021-10-29 20:52:30 -0500

    description spelling error

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description_content = fh.read()
 
 setup(
-    name='cellcount',
-    version='0.0.1',
-    description='Count the number of contiguous regions in a binary array',
+    name="cellcount",
+    version="0.0.1",
+    description="Count the number of contiguous regions in a binary array",
     py_modules=["cellcount"],
     url="https://github.com/ronan-pickell/cell-count",
     author="Ronan Pickell",
     author_email="ronanpickell@gmail.com",
-    package_dir={'': 'src'},
-    long_description=long_description,
-    long_description_context_type='text/markdown',
+    package_dir={"": "src"},
+    long_description=long_description_content,
+    long_description_content_type="text/markdown",
     install_requires = [
         "numpy ~= 1.19"
     ],
@@ -24,5 +24,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Natural Language :: English"
-    ]
+    ],
 )
